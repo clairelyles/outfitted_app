22dd325fc992fa82d044361037f5ecb62e406c2c
clairelyles <claire.lyles@gmail.com>
2014-12-18 19:58:09 -0800

    added testing page

--- insert.sql
+++ insert.sql
@@ -1,6 +1,6 @@
 \connect outfitted
 
-INSERT INTO piecetypes VALUES (1,'blouse','now','now');
+INSERT INTO piecetypes VALUES (1,'shirt','now','now');
 
 INSERT INTO piecetypes VALUES (2,'jacket','now','now');
 
@@ -10,14 +10,22 @@ INSERT INTO piecetypes VALUES (4,'skirt','now','now');
 
 INSERT INTO piecetypes VALUES (5,'pants','now','now');
 
-INSERT INTO tags VALUES (1,'sleepy','Go get em tiger','now','now');
+INSERT INTO tags VALUES (1,'day to night','Go get em tiger','now','now');
 
-INSERT INTO tags VALUES (2,'sexy','Go get em tiger','now','now');
+INSERT INTO tags VALUES (2,'chill','Go get em tiger','now','now');
 
-INSERT INTO tags VALUES (3,'work','Go get em tiger','now','now');
+INSERT INTO tags VALUES (3,'sexy','Go get em tiger','now','now');
 
-INSERT INTO tags VALUES (4,'school','Go get em tiger','now','now');
+INSERT INTO tags VALUES (4,'funky','Go get em tiger','now','now');
 
-INSERT INTO tags VALUES (5,'sassy','Go get em tiger','now','now');
+INSERT INTO tags VALUES (5,'whimsical','Go get em tiger','now','now');
 
-INSERT INTO tags VALUES (6,'asif','Go get em tiger','now','now');
+INSERT INTO tags VALUES (6,'work','Go get em tiger','now','now');
+
+INSERT INTO tags VALUES (7,'school','Go get em tiger','now','now');
+
+INSERT INTO tags VALUES (8,'sassy','Go get em tiger','now','now');
+
+INSERT INTO tags VALUES (9,'asif','Go get em tiger','now','now');
+
+INSERT INTO tags VALUES (10,'date','Go get em tiger','now','now');
